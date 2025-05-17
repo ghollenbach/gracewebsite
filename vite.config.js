c23a19b795595c620929dce5a4a1fe489240296f
Grace Hollenbach <llamaladycreates@gmail.com>
2025-05-17 16:34:15 -0400

    github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/repo-name/',  // Replace 'repo-name' with your actual GitHub repo name
+  base: '/gracewebsite/',
   plugins: [react()]
 })
