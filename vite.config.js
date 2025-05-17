59ba4f6314f2527f7e9acf1f820339034888c12b
Grace Hollenbach <llamaladycreates@gmail.com>
2025-05-17 16:27:26 -0400

    fixed json file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
+  base: '/repo-name/',  // Replace 'repo-name' with your actual GitHub repo name
+  plugins: [react()]
 })
